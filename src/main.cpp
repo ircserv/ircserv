ca163392cadc79a554b7752604a60ad2ea96cce5
nerdchanii <nerdchanii@gmail.com>
2025-02-18 11:38:35 +0900

    feat: define command keys

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yechakim <yechakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 07:32:10 by yechakim          #+#    #+#             */
-/*   Updated: 2025/02/17 11:55:08 by yechakim         ###   ########.fr       */
+/*   Updated: 2025/02/17 12:45:40 by yechakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 int main(){
   IRCServer &server = IRCServer::getInstance();
-  server.on("CAP", IRCCommand::cap);
-  server.on("JOIN", IRCCommand::join);
-  server.on("PASS", IRCCommand::pass);
-  server.on("NICK", IRCCommand::nick);
-  server.on("USER", IRCCommand::user);
+  server.on(CMD_CAP, IRCCommand::cap);
+  server.on(CMD_JOIN, IRCCommand::join);
+  server.on(CMD_PASS, IRCCommand::pass);
+  server.on(CMD_NICK, IRCCommand::nick);
+  server.on(CMD_USER, IRCCommand::user);
   server.start(6667);
   server.destroy();
   return 0;
--- src/server/IRCServer.hpp
+++ src/server/IRCServer.hpp
@@ -9,12 +9,6 @@
 #include "../parser/Message.hpp"
 #include "../parser/Parser.hpp"
 
-#define CMD_JOIN "JOIN"
-#define CMD_PASS "PASS"
-#define CMD_NICK "NICK"
-#define CMD_USER "USER"
-#define CMD_CAP "CAP"
-
 class User;
 class Channel;
 class UserRepository;
